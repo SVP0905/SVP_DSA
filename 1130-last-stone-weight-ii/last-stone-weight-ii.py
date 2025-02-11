51274d49c78742042849142c8c4a0578aa07b850
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-02-11 17:23:16 +0530

    Time: 15 ms (72.31%) | Memory: 18.3 MB (44.20%) - LeetSync

---
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        total=sum(stones)
+        stones=sorted(stones)
+        target=total//2
+
+        dp=[[False]*(target+1) for _ in range(len(stones)+1)]
+        dp[0][0]=True
+
+        for i in range(1,len(stones)+1):
+            for j in range(target+1):
+                if j<stones[i-1]:
+                    dp[i][j]=dp[i-1][j]
+                else:
+                    dp[i][j]=dp[i-1][j] or dp[i-1][j-stones[i-1]]
+        
+        for j in range(target,-1,-1):
+            if dp[len(stones)][j]:
+                return total-2*j
+        
+        return total
+
+
+        
