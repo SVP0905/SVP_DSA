1f70710693eda3da0ab2c2f8a4fe92d5864ae231
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-02-13 13:18:17 +0530

    Time: 175 ms (71.18%) | Memory: 18.2 MB (64.08%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,16 +1,16 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        m,n=len(coins)+1,amount+1
-        dp=[[0]*n for _ in range(m)]
-        
+        m,n=len(coins),amount+1
+        dp=[0]*n
+        dp[0]=1
+
         for i in range(m):
-            dp[i][0]=1
-        
-        for i in range(1,m):
+            new_dp=dp.copy()
             for j in range(1,n):
-                dp[i][j]+=dp[i-1][j]
-                if j>=coins[i-1]:
-                    dp[i][j]+=dp[i][j-coins[i-1]]
+                if j>=coins[i]:
+                    new_dp[j]+=new_dp[j-coins[i]]
+            dp=new_dp
         
-        return dp[m-1][n-1]
+        return dp[n-1]
+
         
