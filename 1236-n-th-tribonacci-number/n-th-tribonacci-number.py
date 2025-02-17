16e1ca7c44f115c5435a78ef4d27452dca17ef5c
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-02-17 11:26:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (30.35%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -4,6 +4,7 @@ class Solution:
             return 0
         if n==1 or n==2:
             return 1
+            
         t1,t2,t3=0,1,1
         for i in range(3,n+1):
             t1,t2,t3=t2,t3,t1+t2+t3
