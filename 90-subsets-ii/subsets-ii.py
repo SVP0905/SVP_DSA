7444687188270ec4ea08699419915b8f6f079d7d
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-02-21 12:23:59 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (37.63%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -2,20 +2,21 @@ class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         res=[]
+
         def dfs(i,list_):
             if i>=len(nums):
                 res.append(list_.copy())
-                return 
+                return
+            
             list_.append(nums[i])
             dfs(i+1,list_)
             list_.remove(nums[i])
-            i_=i+1
-            while i_<len(nums) and nums[i_]==nums[i]:
-                i_+=1
-            dfs(i_,list_)
+
+            while i+1<len(nums) and nums[i]==nums[i+1]:
+                i+=1
+
+            dfs(i+1,list_)
+        
         dfs(0,[])
 
         return res
-
-
-        
