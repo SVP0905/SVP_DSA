8150a129c38748a805d1a44771c96df318e2cc7a
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-02-21 19:24:39 +0530

    Time: 431 ms (32.30%) | Memory: 25.6 MB (71.99%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -2,7 +2,6 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         m,n=len(s)+1,len(p)+1
         dp=[[False]*n for _ in range(m)]
-
         dp[0][0]=True
 
         for j in range(1,n):
@@ -13,7 +12,7 @@ class Solution:
             for j in range(1,n):
                 if p[j-1]=="*":
                     dp[i][j]=dp[i-1][j] or dp[i][j-1]
-                if s[i-1]==p[j-1] or p[j-1]=="?":
+                if s[i-1]==p[j-1] or p[j-1]=='?':
                     dp[i][j]=dp[i-1][j-1]
         
         return dp[m-1][n-1]
