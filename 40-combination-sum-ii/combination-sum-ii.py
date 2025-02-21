c81098c5b58212392492e60e8b90ccbe94c4dcdd
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-02-21 15:22:31 +0530

    Time: 31 ms (28.10%) | Memory: 17.8 MB (83.57%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -5,18 +5,20 @@ class Solution:
         def dfs(i,target,list_):
             if target==0:
                 res.append(list_.copy())
-                return 
+                return
             if target<0 or i>=n:
                 return
             list_.append(candidates[i])
+            # take the current element 
             dfs(i+1,target-candidates[i],list_)
             list_.pop()
 
             while i+1<n and candidates[i]==candidates[i+1]:
                 i+=1
+
+            #don't take the current element
             dfs(i+1,target,list_)
         
         res=[]
         dfs(0,target,[])
-
         return res
