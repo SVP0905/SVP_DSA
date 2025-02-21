588ba815f0336cea25113bc6bcef7d63da5e0852
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-02-21 16:43:36 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (86.34%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        def dfs(i,target,list_):
+            if target==0 and len(list_)==k:
+                res.append(list_.copy())
+                return 
+            if target<0 or len(list_)>=k or i>9:
+                return
+            
+            for j in range(i,10):
+                list_.append(j)
+                dfs(j+1,target-j,list_)
+                list_.pop()
+        
+        res=[]
+        dfs(1,n,[])
+        return res
