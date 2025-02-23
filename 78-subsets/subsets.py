6255a95c4cc4e44e6789f3b3cbcb600e8bbd4a5d
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-02-23 14:26:50 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (21.24%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,14 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res=[]
-        def dfs(i,list_):
-            if i==len(nums):
-                res.append(list_.copy())
+        def dfs(i,sub_):
+            if i>=len(nums):
+                res.append(sub_.copy())
                 return
-            list_.append(nums[i])
-            dfs(i+1,list_)
-            list_.remove(nums[i])
-            dfs(i+1,list_)
+            sub_.append(nums[i])
+            dfs(i+1,sub_)
+            sub_.pop()
+            dfs(i+1,sub_)
+
+        res=[]
         dfs(0,[])
 
         return res
