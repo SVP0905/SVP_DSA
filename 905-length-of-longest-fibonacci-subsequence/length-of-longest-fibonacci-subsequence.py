c0ef7e42180a6b7966b35966a955269ba0337564
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-02-27 14:02:17 +0530

    Time: 1305 ms (38.74%) | Memory: 20.4 MB (36.09%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.py
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.py
@@ -1,26 +1,25 @@
 class Solution:
     def lenLongestFibSubseq(self, arr: List[int]) -> int:
         n=len(arr)
+
         if n<3:
             return 0
-            
+        
+        max_=0
         map_={arr[i]:i for i in range(n)}
 
-        max_=0
-
-        def dfs(i,j,cnt):
+        def dfs(i,j,len_):
             nonlocal max_
             next_val=arr[i]+arr[j]
 
             if next_val in map_ and map_[next_val]>j:
-                dfs(j,map_[next_val],cnt+1)
+                dfs(j,map_[next_val],len_+1)
             
-            if cnt>=3:
-                max_=max(max_,cnt)
+            if len_>=3:
+                max_=max(max_,len_)
 
         for i in range(n-2):
             for j in range(i+1,n-1):
                 dfs(i,j,2)
         
         return max_
-             
