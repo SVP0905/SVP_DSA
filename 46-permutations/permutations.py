c102022242aa5142835dad227371a2d95d56e393
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-03-02 19:33:25 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (56.67%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,20 +1,21 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def dfs(list_,map_):
-            if len(list_)==len(nums):
-                res.append(list_.copy())
+        def dfs(stack_,map_):
+            if len(stack_)==len(nums):
+                res.append(stack_.copy())
                 return
             
             for i in range(len(nums)):
                 if map_[i]==1:
                     continue
                 map_[i]=1
-                list_.append(nums[i])
-                dfs(list_,map_)
-                list_.pop()
+                stack_.append(nums[i])
+                dfs(stack_,map_)
+                stack_.pop()
                 map_[i]=0
         
         res=[]
         map_=[0]*len(nums)
-        dfs([],map_)
+        stack_=[]
+        dfs(stack_,map_)
         return res
