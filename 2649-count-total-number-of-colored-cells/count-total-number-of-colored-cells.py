f9b5afb9e2b5956827db27c324c113864e95f26d
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-03-05 19:06:13 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (94.74%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.py
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.py
@@ -1,9 +1,3 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        blue_cells=1
-        addend=4
-        while n-1:
-            blue_cells+=addend
-            addend+=4
-            n-=1
-        return blue_cells
+        return 1+2*(n-1)*n
