520764bf59be4e00967499b53db9d588197e19ca
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-03-22 01:31:42 +0530

    Time: 23 ms (96.92%) | Memory: 19.7 MB (79.98%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
@@ -1,23 +1,35 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
-        cnt=len(recipes)
-        visited=[False]*cnt
         supplies=set(supplies)
+        recipe_to_idx = {recipe: i for i, recipe in enumerate(recipes)}
+        adj={i:[] for i in range(len(recipes))}
+        indegree=[0]*len(recipes)
         res=[]
-        for _ in range(cnt):
-            for i in range(cnt):
-                if visited[i]:
-                    continue
-                flag=True
-                for j in range(len(ingredients[i])):
-                    if ingredients[i][j] not in supplies:
-                        flag=False
-                        break
-                
-                if flag:
-                    res.append(recipes[i])
-                    visited[i]=True
-                    supplies.add(recipes[i])
+
+        for i in range(len(recipes)):
+            for ingredient in ingredients[i]:
+                if ingredient not in supplies:
+                    if ingredient in recipe_to_idx:
+                        j=recipe_to_idx[ingredient]
+                        adj[j].append(i)
+                        indegree[i]+=1
+                    else:
+                        indegree[i]=1000
         
+        q=deque()
+        for i in range(len(recipes)):
+            if indegree[i]==0:
+                q.append(i)
+        
+        while q:
+            node=q.popleft()
+            res.append(recipes[node])
+            for nei in adj[node]:
+                indegree[nei]-=1
+                if indegree[nei]==0:
+                    q.append(nei)
+        
+                
         return res
-            
+        
+        
