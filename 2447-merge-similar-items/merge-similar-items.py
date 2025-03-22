f8bd6a4ecc706f08feb3b72cf2ee5d9be2aacb1c
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-03-22 19:11:28 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (95.69%) - LeetSync

--- 2447-merge-similar-items/merge-similar-items.py
+++ 2447-merge-similar-items/merge-similar-items.py
@@ -1,13 +1,14 @@
 class Solution:
     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
-        val_to_wei={}
+        map_={}
         for val,wei in items1:
-            val_to_wei[val]=val_to_wei.get(val,0)+wei
+            map_[val]=map_.get(val,0)+wei
         
         for val,wei in items2:
-            val_to_wei[val]=val_to_wei.get(val,0)+wei
+            map_[val]=map_.get(val,0)+wei
+        
+        res=[[val,wei] for val,wei in map_.items()]
 
-        res=[[val,wei] for val,wei in val_to_wei.items()]
         res.sort(key=lambda x:x[0])
 
         return res
