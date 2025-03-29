22be66e6cc7666f3e47a7140ba25f9e8e25a0529
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-03-29 17:02:12 +0530

    Time: 18 ms (13.50%) | Memory: 17.9 MB (96.24%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -6,14 +6,14 @@ class Solution:
             for j in range(n):
                 map_[grid[i][j]]+=1
         
-        res=[]
+        ans=[]
         for i in range(1,n**2+1):
             if map_[i] and map_[i]==2:
-                res.append(i)
+                ans.append(i)
                 break
-        
+
         for i in range(1,n**2+1):
-            if map_[i]==0:
-                res.append(i)
+            if not map_[i]:
+                ans.append(i)
                 break
-        return res
+        return ans
