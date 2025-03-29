23a38ef6a1799874802b9aeb196a672b6abee708
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-03-29 16:39:09 +0530

    Time: 11 ms (34.24%) | Memory: 18.2 MB (58.98%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -1,25 +1,19 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
         n=len(grid)
-        map_={}
+        map_=defaultdict(int)
         for i in range(n):
             for j in range(n):
-                if grid[i][j] not in map_:
-                    map_[grid[i][j]]=1
-                else:
-                    map_[grid[i][j]]+=1
+                map_[grid[i][j]]+=1
         
         res=[]
         for i in range(1,n**2+1):
-            if i in map_ and map_[i]==2:
+            if map_[i] and map_[i]==2:
                 res.append(i)
                 break
         
         for i in range(1,n**2+1):
-            if i not in map_:
+            if map_[i]==0:
                 res.append(i)
                 break
-
         return res
-        
-        
