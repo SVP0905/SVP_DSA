e0fbbe989a5fd0e68995762b3e1f585feb38c5eb
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-04-01 19:14:20 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (8.50%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -2,18 +2,11 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         memo={}
         def dfs(i):
-            if i==0:
-                return 1
-            if i==1:
+            if i==0 or i==1:
                 return 1
             if i in memo:
                 return memo[i]
-
-            left=dfs(i-1)
-            right=dfs(i-2)
-
-            memo[i]=left+right 
-
+            
+            memo[i]=dfs(i-1)+dfs(i-2)
             return memo[i]
-        
         return dfs(n)
