3c6d159525a35f7903d49e463e71ab7188a9c65e
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-04-11 10:23:37 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (71.99%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,32 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        res=[0]*(m+n)
-        l,r,i=0,0,0
-
-        while l<m and r<n:
-            if nums1[l]<=nums2[r]:
-                res[i]=nums1[l]
-                i+=1
-                l+=1
+        l,r,i=m-1,n-1,m+n-1
+        
+        while l>=0 and r>=0:
+            if nums1[l]>nums2[r]:
+                nums1[i]=nums1[l]
+                l-=1
             else:
-                res[i]=nums2[r]
-                i+=1
-                r+=1
+                nums1[i]=nums2[r]
+                r-=1
+            i-=1
         
-        while l<m:
-            res[i]=nums1[l]
-            l+=1
-            i+=1
-        
-        while r<n:
-            res[i]=nums2[r]
-            r+=1
-            i+=1
-        
-        for i in range(m+n):
-            nums1[i]=res[i]
-        
-
-
+        while r>=0:
+            nums1[i]=nums2[r]
+            r-=1
+            i-=1
         
