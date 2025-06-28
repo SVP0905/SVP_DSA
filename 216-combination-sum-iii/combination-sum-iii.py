368caa9332243c64fda4adb72c7acccb52a36615
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-06-28 23:13:46 +0530

    Time: 1 ms (34.63%) | Memory: 17.9 MB (15.72%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -1,7 +1,6 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         res=[]
-
         def dfs(i,sum_,arr):
             if sum_==n and len(arr)==k:
                 res.append(arr.copy())
@@ -9,10 +8,11 @@ class Solution:
             
             if sum_>n or len(arr)>k or i>9:
                 return
-            
             arr.append(i)
             dfs(i+1,sum_+i,arr)
+
             arr.pop()
             dfs(i+1,sum_,arr)
+        
         dfs(1,0,[])
         return res
