341135ac003d3b2b4a382053b6a20b178f08c8b5
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-07-01 23:35:16 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (80.79%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        first_row=set('qwertyuiop')
+        second_row=set('asdfghjkl')
+        third_row=set('zxcvbnm')
+        res=[]
+
+        for word in words:
+            word_chars=set(word.lower())
+            if word_chars.issubset(first_row):
+                res.append(word)
+            elif word_chars.issubset(second_row):
+                res.append(word)
+            elif word_chars.issubset(third_row):
+                res.append(word)
+        
+        return res
+        
+                
