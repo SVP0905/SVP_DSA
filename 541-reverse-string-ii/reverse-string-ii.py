8242341f4e1993647d9eb735e3ad734cb2d6a177
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-07-02 00:25:14 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (24.07%) - LeetSync

---
+++ 541-reverse-string-ii/reverse-string-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        n=len(s)
+        res=[]
+        for i in range(0,n,2*k):
+            segment=s[i:i+2*k]
+            if len(segment)<k:
+                rev_part=segment[::-1]
+                remaining_part=''
+            else:
+                rev_part=segment[:k][::-1]
+                remaining_part=segment[k:]
+            res.append(rev_part+remaining_part)
+        
+        return ''.join(res)
