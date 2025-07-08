417059668006e70eaa99821e62928d58fe182159
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-07-08 23:03:43 +0530

    Time: 1157 ms (37.91%) | Memory: 239.9 MB (29.12%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.py
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.py
@@ -2,8 +2,9 @@ class Solution:
     def maxValue(self, events: List[List[int]], k: int) -> int:
         events.sort()
         n=len(events)
-        
-        next_valid_evnts=[n]*n
+
+        next_valid_events=[n]*n
+
         for i in range(n):
             target=events[i][1]+1
             l,r=i+1,n
@@ -13,19 +14,17 @@ class Solution:
                     r=mid
                 else:
                     l=mid+1
-            next_valid_evnts[i]=l
+            next_valid_events[i]=l
+        
 
         @lru_cache(maxsize=None)
-        def dfs(i,evnts_cnt):
-            if i>=n or evnts_cnt>=k:
+        def dfs(i,events_attended):
+            if i>=n or events_attended>=k:
                 return 0
             
-            not_choose=dfs(i+1,evnts_cnt)
-
-            choose=0
-            current_end=events[i][1]
+            not_choose=dfs(i+1,events_attended)
             
-            choose=events[i][2]+dfs(next_valid_evnts[i],evnts_cnt+1)
+            choose=events[i][2]+dfs(next_valid_events[i],events_attended+1)
 
-            return max(not_choose,choose)
+            return max(choose,not_choose)
         return dfs(0,0)
