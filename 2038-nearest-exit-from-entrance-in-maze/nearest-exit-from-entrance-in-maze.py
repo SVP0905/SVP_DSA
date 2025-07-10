29967573a5ea03c60b0b4b5c8195940b5fe60a6a
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-07-10 20:39:22 +0530

    Time: 81 ms (57.91%) | Memory: 19.7 MB (55.07%) - LeetSync

---
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
@@ -0,0 +1,25 @@
+class Solution:
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        m,n=len(maze),len(maze[0])
+        q=deque([entrance])
+
+        directions=[(1,0),(0,1),(-1,0),(0,-1)]
+        visited=set()
+        visited.add((entrance[0],entrance[1]))
+        steps=0
+        while q:
+            q_len=len(q)
+        
+            for _ in range(q_len):
+                x,y=q.popleft()
+                if (x==0 or x==m-1 or y==0 or y==n-1) and [x,y]!=entrance:
+                    return steps
+                for dx,dy in directions:
+                    new_x,new_y=x+dx,y+dy
+                    if (0<=new_x<m and 0<=new_y<n and maze[new_x][new_y]=='.' and (new_x,new_y) not in visited):
+                        q.append((new_x,new_y))
+                        visited.add((new_x,new_y))
+            steps+=1
+        
+        return steps if steps==0 else -1
+                    
