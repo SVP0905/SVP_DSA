2ff2620033d5e1e9cf2b11b5f6ba055171e96a6e
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-07-10 15:49:00 +0530

    Time: 299 ms (43.93%) | Memory: 42.6 MB (24.28%) - LeetSync

--- 3741-reschedule-meetings-for-maximum-free-time-ii/reschedule-meetings-for-maximum-free-time-ii.py
+++ 3741-reschedule-meetings-for-maximum-free-time-ii/reschedule-meetings-for-maximum-free-time-ii.py
@@ -1,35 +1,34 @@
 class Solution:
     def maxFreeTime(self, eventTime: int, startTime: List[int], endTime: List[int]) -> int:
-        events=sorted(list(zip(startTime,endTime)))
-
+        events=list(zip(startTime,endTime))
+        n=len(events)
         free_time_arr=[]
         prev_end=0
-        for start,end in events:
-            free_time_arr.append(start-prev_end)
-            prev_end=end
-        
+        for i in range(n):
+            free_time_arr.append(events[i][0]-prev_end)
+            prev_end=events[i][1]
         free_time_arr.append(eventTime-prev_end)
 
-        n=len(events)
         maxRight=[0]*(n+1)
         for i in range(n-1,-1,-1):
             maxRight[i]=max(maxRight[i+1],free_time_arr[i+1])
-        
+
         maxLeft=[0]*(n+1)
         for i in range(1,n+1):
             maxLeft[i]=max(maxLeft[i-1],free_time_arr[i-1])
         
-
         max_=max(free_time_arr)
         for i in range(n):
             d=events[i][1]-events[i][0]
-            f1,f2=free_time_arr[i],free_time_arr[i+1]
+            f1=free_time_arr[i]
+            f2=free_time_arr[i+1]
             merged_gap=f1+d+f2
 
-            max_alt_gap=max(maxLeft[i],maxRight[i+1])
-
+            max_alt_gap=max(maxRight[i+1],maxLeft[i])
             if max_alt_gap<d:
                 max_=max(max_,merged_gap-d)
             else:
                 max_=max(max_,merged_gap)
         return max_
+
+        
