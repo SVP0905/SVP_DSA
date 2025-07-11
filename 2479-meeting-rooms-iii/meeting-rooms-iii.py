46620ba6e728c74e4b25436b60d6ce5e0e0a3c89
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-07-11 16:12:07 +0530

    Time: 177 ms (64.66%) | Memory: 51.7 MB (14.86%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.py
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.py
@@ -5,21 +5,20 @@ class Solution:
 
         busy_rooms=[]
         meetings_per_room=[0]*n
-
         for start,end in sorted(meetings):
             while busy_rooms and busy_rooms[0][0]<=start:
-                end_time,room_no=heapq.heappop(busy_rooms)
-                heapq.heappush(available_rooms,room_no)
+                cur_end,cur_room=heapq.heappop(busy_rooms)
+                heapq.heappush(available_rooms,cur_room)
             
             if available_rooms:
                 choosen_room=heapq.heappop(available_rooms)
                 heapq.heappush(busy_rooms,(end,choosen_room))
             else:
                 earliest_end,earliest_room=heapq.heappop(busy_rooms)
-                d=end-start
-                new_end=earliest_end+d
+                new_end=earliest_end+end-start
                 heapq.heappush(busy_rooms,(new_end,earliest_room))
                 choosen_room=earliest_room
+            
             meetings_per_room[choosen_room]+=1
         
         return meetings_per_room.index(max(meetings_per_room))
