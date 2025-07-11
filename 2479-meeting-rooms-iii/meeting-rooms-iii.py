5687d87af355c697907085406425a2b9eb9bf412
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-07-11 15:07:45 +0530

    Time: 586 ms (9.50%) | Memory: 51.6 MB (34.22%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.py
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.py
@@ -1,26 +1,25 @@
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-        when_room_become_free=[0]*n
+        when_rooms_become_free=[0]*n
         meetings_per_room=[0]*n
 
         for start,end in sorted(meetings):
             d=end-start
             assigned_room=None
+
             for i in range(n):
-                if when_room_become_free[i]<=start:
-                    when_room_become_free[i]=end
+                if when_rooms_become_free[i]<=start:
+                    when_rooms_become_free[i]=end
                     assigned_room=i
                     break
-                
+            
             if assigned_room is None:
-                earliest_time=min(when_room_become_free)
-                assigned_room=when_room_become_free.index(earliest_time)
+                earliest_time=min(when_rooms_become_free)
+                assigned_room=when_rooms_become_free.index(earliest_time)
 
-                when_room_become_free[assigned_room]=earliest_time+d
+                when_rooms_become_free[assigned_room]=earliest_time+d
             
             meetings_per_room[assigned_room]+=1
         
         i=max(meetings_per_room)
         return meetings_per_room.index(i)
-
-                
