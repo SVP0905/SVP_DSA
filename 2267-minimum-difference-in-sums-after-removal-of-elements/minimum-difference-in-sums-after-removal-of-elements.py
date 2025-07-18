dd9061f006ab03348f326a862e3a41a09f3bf90e
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-07-19 00:40:19 +0530

    Time: 583 ms (25.15%) | Memory: 57.8 MB (8.77%) - LeetSync

--- 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.py
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.py
@@ -1,54 +1,38 @@
 class Solution:
     def minimumDifference(self, nums: List[int]) -> int:
-        N = len(nums)
-        n = N // 3
-        min_sums = [0] * (N + 1)
-        max_sums = [0] * (N + 1)
+        N=len(nums)
+        n=N//3
+        minheap=[]
+        maxheap=[]
+        leftMinSum=[0]*N
+        rightMaxSum=[0]*N
 
-        max_heap_l = []
-        sum_l = 0
-
-        for i in range(N):
-            heapq.heappush(max_heap_l, -nums[i])
-            sum_l += nums[i]
-
-            if len(max_heap_l) > n:
-                removed = -heapq.heappop(max_heap_l)
-                sum_l -= removed 
-
-            if len(max_heap_l) == n:
-                min_sums[i + 1] = sum_l
-
-        min_heap_r = []
-        sum_r = 0
-
-        for i in range(N-1, -1, -1):
-            heapq.heappush(min_heap_r, nums[i])
-            sum_r += nums[i]
-
-            if len(min_heap_r) > n:
-                removed = heapq.heappop(min_heap_r)
-                sum_r -= removed  
-
-            if len(min_heap_r) == n:
-                max_sums[i] = sum_r
-        
-        
-
-        # print(max_sums)
-        # print(min_sums)
-
-        min_diff = float("inf")
-
-        for i in range(n, 2*n + 1):
-
-            min_diff = min(min_diff, min_sums[i] - max_sums[i])
-
-        return min_diff
-        
-       
-
-
+        leftSum=0
+        for i in range(N-n):
+            heapq.heappush(maxheap,-nums[i])
+            leftSum+=nums[i]
+            if len(maxheap)>n:
+                leftSum-=-heapq.heappop(maxheap)
             
-
         
+            leftMinSum[i]=leftSum
+        
+        
+        rightSum=0
+        for i in range(N-1,n-1,-1):
+            heapq.heappush(minheap,nums[i])
+            rightSum+=nums[i]
+            if len(minheap)>n:
+                rightSum-=heapq.heappop(minheap)
+            
+            rightMaxSum[i]=rightSum
+    
+        
+        print(leftMinSum)
+        print(rightMaxSum)
+        min_=float('inf')
+        for i in range(n-1,2*n):
+            min_= min(min_,leftMinSum[i]-rightMaxSum[i+1])
+        
+        return min_
+                
