f53f01306b8df68ea3765746fd1d82da563ece5a
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-07-18 11:43:30 +0530

    Time: 2425 ms (35.14%) | Memory: 25.8 MB (27.57%) - LeetSync

--- 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.py
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.py
@@ -1,11 +1,29 @@
 class Solution:
     def maximumLength(self, nums: List[int], k: int) -> int:
-        dp = [[0] * k for _ in range(k)]
-        res = 0
-        for num in nums:
-            num %= k
-            for prev in range(k):
-                dp[prev][num] = dp[num][prev] + 1
-                res = max(res, dp[prev][num])
-        return res
+        
+        # def solve_dfs(val):
+        #     dp=[1]*len(nums)
+        #     for i in range(1,len(nums)):
+        #         for j in range(i):
+        #             if (nums[i]+nums[j])%k==val:
+        #                 dp[i]=max(dp[i],dp[j]+1)
             
+        #     return max(dp)
+
+        # max_=0
+        # for i in range(k):
+        #     max_=max(max_,solve_dfs(i))
+        # return max_
+
+        n=len(nums)
+        dp=[[1]*n for _ in range(k)]
+        
+        max_=0
+
+        for i in range(1,n):
+            for j in range(i):
+                val=(nums[i]+nums[j])%k
+                dp[val][i]=max(dp[val][i],dp[val][j]+1)
+                max_=max(max_,dp[val][i])
+        return max_
+        
