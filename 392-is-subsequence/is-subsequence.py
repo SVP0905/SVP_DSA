a62d36c391a539e8926a461dda00ed6a8ffc20b7
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-07-30 02:17:54 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (24.03%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,14 +1,10 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        def dfs(s_idx,t_idx):
-            if s_idx==len(s):
-                return True
-            if t_idx==len(t):
-                return False
-            
-            if s[s_idx]==t[t_idx]:
-                return dfs(s_idx+1,t_idx+1)
-            else:
-                return dfs(s_idx,t_idx+1)
+        l,r=0,0
+        while l<len(s) and r<len(t):
+            if s[l]==t[r]:
+                l+=1
+            r+=1
+        return l==len(s)
         
-        return dfs(0,0)
+        
