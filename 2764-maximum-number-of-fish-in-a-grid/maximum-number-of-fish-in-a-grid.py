e209935a0790cdac2ae3d46b53b4fe128e107c59
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-07-29 22:14:48 +0530

    Time: 44 ms (48.46%) | Memory: 18 MB (61.63%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.py
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.py
@@ -1,25 +1,23 @@
 class Solution:
     def findMaxFish(self, grid: List[List[int]]) -> int:
         m,n=len(grid),len(grid[0])
-        max_=0
         visited=set()
+        max_=0
         for r in range(m):
             for c in range(n):
                 if grid[r][c]>0 and (r,c) not in visited:
-                    cur=0
-                    q=deque([(r,c)])
                     visited.add((r,c))
+                    q=deque([(r,c)])
+                    cur=0
                     while q:
                         x,y=q.popleft()
                         cur+=grid[x][y]
-                        directions=[(0,1),(1,0),(0,-1),(-1,0)]
-                        for dr,dc in directions:
+                        for dr,dc in [[0,1],[1,0],[0,-1],[-1,0]]:
                             new_dr,new_dc=dr+x,dc+y
-                            if 0<=new_dr<m and 0<=new_dc<n and grid[new_dr][new_dc] and (new_dr,new_dc) not in visited:
-                                visited.add((new_dr,new_dc))
+                            if 0<=new_dr<m and 0<=new_dc<n and grid[new_dr][new_dc]>0 and (new_dr,new_dc) not in visited:
                                 q.append((new_dr,new_dc))
+                                visited.add((new_dr,new_dc))
                     
-
                     max_=max(max_,cur)
         
         return max_
