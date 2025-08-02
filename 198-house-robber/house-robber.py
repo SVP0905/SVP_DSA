2bc37b978c73305419268f0f689c9aab63edab38
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-08-02 20:19:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (14.75%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -12,7 +12,7 @@ class Solution(object):
         
         dp=[0]*n
         dp[0]=nums[0]
-
-        for i in range(1,n):
+        dp[1]=max(nums[0],nums[1])
+        for i in range(2,n):
             dp[i]=max(dp[i-1],nums[i]+dp[i-2])
         return max(dp)
