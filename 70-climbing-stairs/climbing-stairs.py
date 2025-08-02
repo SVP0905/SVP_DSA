4fc1c41cd1d03655c789dd625c3379191101e3de
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-08-02 14:19:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (53.37%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -4,12 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-
+        if n==1:
+            return 1
+        if n==2:
+            return 2
         dp=[0]*(n+1)
-        dp[0]=1
-
-        for i in range(1,n+1):
+        dp[1]=1
+        dp[2]=2
+        for i in range(3,n+1):
             dp[i]+=dp[i-1]+dp[i-2]
         return dp[n]
-
         
