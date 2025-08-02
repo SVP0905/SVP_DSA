b7f2d52ed31c2b6f0cfed4db83a61ac747bf95de
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-08-02 18:51:11 +0530

    Time: 15 ms (75.96%) | Memory: 12.4 MB (50.25%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -4,10 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        def fib(x):
-            if x==0:
-                return 0
-            if x==1:
-                return 1
-            return fib(x-1)+fib(x-2)
-        return fib(n)
+        if n==0:
+            return 0
+        if n==1:
+            return 1
+            
+        prev,cur=0,1
+
+        for i in range(2,n+1):
+            temp=prev+cur
+            prev,cur=cur,temp
+        return cur
