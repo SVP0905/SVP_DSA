10e4f62444321e86c6858367d48e9df211745d6f
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-08-04 02:53:42 +0530

    Time: 9 ms (49.60%) | Memory: 23.5 MB (15.74%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -6,15 +6,13 @@ class Solution(object):
         :rtype: List[int]
         """
         map_={}
-
-        for i,num in enumerate(nums):
-            diff=target-num
+        def dfs(i,map_):
+            if i>=len(nums):
+                return None
+            
+            diff=target-nums[i]
             if diff in map_:
                 return (i,map_[diff])
-            map_[num]=i
-        
-
-
-        
-
-        
+            map_[nums[i]]=i
+            return dfs(i+1,map_)
+        return dfs(0,map_)
