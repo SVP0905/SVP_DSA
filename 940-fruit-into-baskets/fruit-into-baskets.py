f23ad0ce03670118f4332dabc93651fd27312f2a
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-08-05 11:06:40 +0530

    Time: 178 ms (69.91%) | Memory: 23.5 MB (76.92%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,28 +1,17 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        baskets = defaultdict(int)
-        left = 0
-        max_picked = 0
+        basket={}
+        l,r=0,0
+        n=len(fruits)
+        res=0
+        while r<n:
+            basket[fruits[r]]=1+basket.get(fruits[r],0)
 
-        # Use 'right' to expand the window
-        for right in range(len(fruits)):
-            fruit_type = fruits[right]
-            baskets[fruit_type] += 1
-
-            # If we have more than 2 types of fruit, shrink the window
-            while len(baskets) > 2:
-                left_fruit_type = fruits[left]
-                baskets[left_fruit_type] -= 1
-                
-                # If a fruit count drops to 0, remove it from our baskets
-                if baskets[left_fruit_type] == 0:
-                    del baskets[left_fruit_type]
-                
-                # Move the left pointer to shrink the window
-                left += 1
-
-            # Update the maximum number of fruits picked so far
-            max_picked = max(max_picked, right - left + 1)
-            
-        return max_picked
-            
+            while len(basket)>2:
+                basket[fruits[l]]-=1
+                if basket[fruits[l]]==0:
+                    del basket[fruits[l]]
+                l+=1
+            res=max(res,r-l+1)
+            r+=1
+        return res
