9e4f95886c139480caf4dafad4cc30df193f4213
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-08-05 11:14:34 +0530

    Time: 179 ms (68.58%) | Memory: 23.7 MB (16.47%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,17 +1,18 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        basket={}
         l,r=0,0
         n=len(fruits)
+        map_={}
         res=0
         while r<n:
-            basket[fruits[r]]=1+basket.get(fruits[r],0)
-
-            while len(basket)>2:
-                basket[fruits[l]]-=1
-                if basket[fruits[l]]==0:
-                    del basket[fruits[l]]
+            map_[fruits[r]]=1+map_.get(fruits[r],0)
+            
+            while len(map_)>2:
+                map_[fruits[l]]-=1
+                if map_[fruits[l]]==0:
+                    del map_[fruits[l]]
                 l+=1
             res=max(res,r-l+1)
             r+=1
         return res
+            
