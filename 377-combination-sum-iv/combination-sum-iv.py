b519f15b2095b54aad42e2951fa621558ea2b9c2
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-08-07 17:02:59 +0530

    Time: 43 ms (57.33%) | Memory: 17.9 MB (47.31%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.py
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -1,17 +1,11 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        @cache
-        def dfs(sum_):
-            if sum_==target:
-                return 1
-            
-            if sum_>target:
-                return 0
-            
-            total=0
-            for num in nums:
-                total+=dfs(sum_+num)
-            
-            return total
+        dp=[0]*(target+1)
+        dp[0]=1
 
-        return dfs(0)
+        for target in range(1,target+1):
+            for num in nums:
+                if num<=target:
+                    dp[target]+=dp[target-num]
+        
+        return dp[target]
