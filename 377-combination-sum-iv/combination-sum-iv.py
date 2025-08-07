7595c74f000416b6160cd1b6e6cf5d3e8a7e72c2
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-08-07 16:43:48 +0530

    Time: 50 ms (16.84%) | Memory: 17.9 MB (24.59%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.py
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -1,20 +1,17 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        memo={}
-        def dfs(remaining):
-            if remaining==0:
+        @cache
+        def dfs(sum_):
+            if sum_==target:
                 return 1
             
-            if remaining<0:
+            if sum_>target:
                 return 0
-            if remaining in memo:
-                return memo[remaining]
-
-            count=0
-            for num in nums:
-                count+=dfs(remaining-num)
             
-            memo[remaining]=count
-            return memo[remaining]
-        
-        return dfs(target)
+            total=0
+            for num in nums:
+                total+=dfs(sum_+num)
+            
+            return total
+
+        return dfs(0)
