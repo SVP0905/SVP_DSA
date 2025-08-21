e8364d1778cf14c58629d5449c7c803df4ab04ff
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-08-21 14:32:36 +0530

    Time: 38 ms (57.32%) | Memory: 24.2 MB (47.56%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        m,n=len(matrix),len(matrix[0])
+        heights=[0]*n
+        max_=0
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j]=='1':
+                    heights[j]+=1
+                else:
+                    heights[j]=0
+        
+            max_=max(max_,self.longestRecInHisto(heights))
+        
+        return max_
+
+    def longestRecInHisto(self,heights):
+        heights.append(0)
+        stack=[-1]
+        max_=0
+
+        for i in range(len(heights)):
+            while stack and heights[i]<heights[stack[-1]]:
+                h=heights[stack.pop()]
+                r=i
+                l=stack[-1]
+                area=h*(r-l-1)
+                max_=max(max_,area)
+            stack.append(i)
+            
+        
+        return max_
