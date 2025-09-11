9c596a5b3e23593e417063883f7c75bbcb9d59b5
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-09-11 12:31:08 +0530

    Time: 30 ms (90.22%) | Memory: 24.2 MB (19.49%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.py
+++ 85-maximal-rectangle/maximal-rectangle.py
@@ -1,32 +1,43 @@
 class Solution:
+    def find_max(self,heights):
+        n=len(heights)
+        stack=[]
+        max_=0
+
+        for i in range(n):
+            while stack and heights[i]<heights[stack[-1]]:
+                cur_idx=stack.pop()
+                nse=i
+                pse=stack[-1] if stack else -1
+                height=heights[cur_idx]
+                width=nse-pse-1
+                max_=max(max_,height*width)
+            stack.append(i)
+        
+        while stack:
+            cur_idx=stack.pop()
+            nse=n
+            pse=stack[-1] if stack else -1
+            height=heights[cur_idx]
+            width=nse-pse-1
+            max_=max(max_,height*width)
+        
+        return max_
+
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         m,n=len(matrix),len(matrix[0])
-        heights=[0]*n
+
         max_=0
+        heights=[0]*n
         for i in range(m):
             for j in range(n):
                 if matrix[i][j]=='1':
                     heights[j]+=1
                 else:
                     heights[j]=0
-        
-            max_=max(max_,self.longestRecInHisto(heights))
 
-        return max_
-    
-    def longestRecInHisto(self,heights):
-        heights=heights+[0]
-        stack=[-1]
-        max_=0
-
-        for i in range(len(heights)):
-            while stack and heights[i]<heights[stack[-1]]:
-                h=heights[stack.pop()]
-                r=i
-                l=stack[-1]
-                area=h*(r-l-1)
-                max_=max(max_,area)
-            stack.append(i)
+                
+            max_=max(max_,self.find_max(heights))
         
-        stack.pop()
         return max_
+        
