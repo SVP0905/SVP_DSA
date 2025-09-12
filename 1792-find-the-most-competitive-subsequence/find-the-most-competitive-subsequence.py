97563f9e5f1d8d459f9d9abd9cf0ac6e454d9fb0
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-09-12 17:30:57 +0530

    Time: 90 ms (70.27%) | Memory: 30.7 MB (36.26%) - LeetSync

--- 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.py
+++ 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.py
@@ -4,10 +4,13 @@ class Solution:
         n=len(nums)
 
         for i in range(n):
+            # Only pop if:
+            # 1. Current element is smaller than stack top
+            # 2. We have enough remaining elements to reach size k
             while (stack and nums[i]<stack[-1] and len(stack)+n-i>k):
                 stack.pop()
             
             if len(stack)<k:
                 stack.append(nums[i])
-                
+
         return stack
