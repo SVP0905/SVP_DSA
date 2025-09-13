b5dd72d8cd9d1b95532ba968f2678b6dff9198f8
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-09-13 11:21:22 +0530

    Time: 3 ms (85.15%) | Memory: 18.2 MB (34.65%) - LeetSync

--- 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.py
+++ 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.py
@@ -1,10 +1,15 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
-        sorted_arr=sorted(arr)
         n=len(arr)
-        chunks=0
+        stack=[]
+
         for i in range(n):
-            current_sorted=sorted(arr[0:i+1])
-            if current_sorted==sorted_arr[0:i+1]:
-                chunks+=1
-        return chunks
+            if stack and arr[i]<stack[-1]:
+                cur_stack_max=stack.pop()
+                while stack and arr[i]<stack[-1]:
+                    cur_stack_max=max(cur_stack_max,stack.pop())
+                stack.append(cur_stack_max)
+            else:
+                stack.append(arr[i])
+        
+        return len(stack)
