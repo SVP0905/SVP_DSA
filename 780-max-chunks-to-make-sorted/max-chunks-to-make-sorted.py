850b2e86257b765971fe933dfe885aeb3260297a
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-09-13 11:45:30 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (5.84%) - LeetSync

---
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        stack=[]
+        n=len(arr)
+
+        for i in range(n):
+            if stack and arr[i]<stack[-1]:
+                cur_stack_max=stack.pop()
+                while stack and arr[i]<stack[-1]:
+                    cur_stack_max=max(cur_stack_max,stack.pop())
+                stack.append(cur_stack_max)
+            else:
+                stack.append(arr[i])
+            
+        return len(stack)
