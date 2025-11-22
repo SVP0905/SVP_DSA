2d559cbe584428781a45cf3e67c85844344b9ea4
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-11-23 02:31:52 +0530

    Time: 767 ms (17.47%) | Memory: 59.4 MB (6.55%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
@@ -1,18 +1,16 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
-        def rec(str_):
+        def dnc(str_):
             if len(str_)<k:
                 return 0
+            
             cnt=Counter(str_)
             for key,val in cnt.items():
                 if val<k:
                     pivot=str_.find(key)
 
-                    left=rec(str_[:pivot])
-                    right=rec(str_[pivot+1:])
-
-                    return max(left,right)
+                    return max(dnc(str_[:pivot]),dnc(str_[pivot+1:]))
             
             return len(str_)
         
-        return rec(s)
+        return dnc(s)
