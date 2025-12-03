df25806c4277eeea99550fa99fd751eb7b28b6fe
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-03 12:34:51 +0530

    Time: 27 ms (74.41%) | Memory: 21.3 MB (34.43%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -2,7 +2,6 @@ class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
         map_={0:1}
         cur_sum=0
-
         cnt=0
         for i,n in enumerate(nums):
             cur_sum+=n
@@ -12,6 +11,6 @@ class Solution:
                 cnt+=map_[rem]
                 map_[rem]+=1
             else:
-                map_[rem]=map_.get(rem,0)+1 
+                map_[rem]=map_.get(rem,0)+1
         
         return cnt
