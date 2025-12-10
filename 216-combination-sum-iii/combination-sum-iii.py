7b78f574ce5780c3910f44e7ea7c7a43970a9e67
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-10 23:56:29 +0530

    Time: 3 ms (22.72%) | Memory: 17.7 MB (58.72%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -1,18 +1,27 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         res=[]
-        def dfs(i,sum_,arr):
-            if sum_==n and len(arr)==k:
-                res.append(arr.copy())
+        nums=[i for i in range(1,10)]
+
+        def dfs(start_idx,path):
+            if len(path)==k and sum(path)==n:
+                res.append(path.copy())
                 return
             
-            if sum_>n or len(arr)>k or i>9:
-                return
-            arr.append(i)
-            dfs(i+1,sum_+i,arr)
 
-            arr.pop()
-            dfs(i+1,sum_,arr)
+            for i in range(start_idx,len(nums)):
+                if nums[i]+sum(path)>n:
+                    break
+                
+                if i>start_idx and nums[i]==nums[i-1]:
+                    continue
+
+                path.append(nums[i])
+                dfs(i+1,path)
+                path.pop()
         
-        dfs(1,0,[])
+        dfs(0,[])
+
         return res
+
+            
