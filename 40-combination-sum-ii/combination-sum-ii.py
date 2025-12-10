767e1d4316bd15728a89744b3cfec213e998781d
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-10 20:49:27 +0530

    Time: 3 ms (92.43%) | Memory: 17.6 MB (99.68%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -8,17 +8,18 @@ class Solution:
                 res.append(path.copy())
                 return
             
-            i=start_idx
-            while i<len(candidates):
+            for i in range(start_idx,len(candidates)):
+                    
                 if candidates[i]+sum(path)>target:
                     return
+
+                if i>start_idx and candidates[i]==candidates[i-1]:
+                    continue
                 
                 path.append(candidates[i])
                 dfs(i+1,path)
                 path.pop()
-                while i+1<len(candidates) and candidates[i]==candidates[i+1]:
-                    i+=1
-                i+=1
+
         
         dfs(0,[])
         return res
