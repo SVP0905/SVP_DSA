7128cd761dd5dddac415236806f2ebdd0b6ca4c6
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-11 18:36:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (58.76%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,14 +1,21 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def permute(idx):
-            if idx==len(nums):
-                res.append(nums.copy())
-                return
-            for i in range(idx,len(nums)):
-                nums[i],nums[idx]=nums[idx],nums[i]
-                permute(idx+1)
-                nums[i],nums[idx]=nums[idx],nums[i]
-        
         res=[]
-        permute(0)
+        info=[False]*len(nums)
+        def dfs(depth,path):
+            if depth==len(nums):
+                res.append(path.copy())
+                return
+            
+
+            for i in range(len(nums)):
+                if not info[i]:
+                    path.append(nums[i])
+                    info[i]=True
+                    dfs(depth+1,path)
+                    path.pop()
+                    info[i]=False
+        
+        dfs(0,[])
+
         return res
