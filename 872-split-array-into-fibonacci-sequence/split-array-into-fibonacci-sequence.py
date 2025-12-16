ac37c153aab5e52df98e4e61a53ca2cf04c4ab54
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-17 00:58:56 +0530

    Time: 7 ms (55.56%) | Memory: 17.6 MB (95.14%) - LeetSync

---
+++ 872-split-array-into-fibonacci-sequence/split-array-into-fibonacci-sequence.py
@@ -0,0 +1,43 @@
+class Solution:
+    def splitIntoFibonacci(self, num: str) -> List[int]:
+        res=[]
+
+
+        def dfs(start_i,path):
+            if start_i>=len(num):
+                return len(path)>=3
+                
+
+            
+            for j in range(start_i,len(num)):
+                chunk=num[start_i:j+1]
+
+                if chunk[0]=='0' and len(chunk)>1:
+                    return
+                
+                val=int(chunk)
+                if val>2**31-1:
+                    return
+                
+                if len(path)>=2:
+                    needed=path[-1]+path[-2]
+                    if val<needed:
+                        continue
+                    if val>needed:
+                        break
+                
+                path.append(val)
+
+                if dfs(j+1,path):
+                    return True
+                
+                path.pop()
+
+            return False
+        
+        
+        if dfs(0,res):
+            return res
+        else:
+            return []
+
