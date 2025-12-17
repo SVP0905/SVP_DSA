b576813a7cd0e0359092b1637a7d7ef7c29120b1
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-17 18:33:18 +0530

    Time: 1 ms (23.61%) | Memory: 17.7 MB (84.98%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
@@ -1,14 +1,29 @@
 class Solution:
     def smallestNumber(self, pattern: str) -> str:
-        res=[]
-        stack=[]
-        n=len(pattern)
+        def dfs(i,path):
+            if i>=len(pattern):
+                return ''.join(map(str,path))
+            
 
-        for i in range(n+1):
-            stack.append(i+1)
-
-            if i==n or pattern[i]=='I':
-                while stack:
-                    res.append(str(stack.pop()))
+            for j in range(1,10):
+                if j in path:
+                    continue
+                
+                if not path:
+                    path.append(j)
+                    res=dfs(i,path) 
+                    if res: return res
+                    path.pop()
+                else:
+                    if pattern[i]=='I' and j>path[-1]:
+                        path.append(j)
+                        res=dfs(i+1,path)
+                        if res: return res
+                        path.pop()
+                    elif pattern[i]=='D' and path[-1]>j:
+                        path.append(j)
+                        res=dfs(i+1,path)
+                        if res: return res
+                        path.pop()
         
-        return ''.join(res)
+        return dfs(0,[])
