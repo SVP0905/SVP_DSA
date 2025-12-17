fc150b38b25d2866fc016f67e576e3ffbdf05f3d
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-17 20:54:29 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (8.39%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -7,12 +7,11 @@ class Solution:
             return 1
 
         dp=[0]*(n+1)
-        dp[0]=0
-        dp[1]=1
-        dp[2]=1
+        t1,t2,t3=0,1,1
 
         for i in range(3,n+1):
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
+            temp=t1+t2+t3
+            t1,t2,t3=t2,t3,temp
         
-        return dp[n]
+        return t3
         
