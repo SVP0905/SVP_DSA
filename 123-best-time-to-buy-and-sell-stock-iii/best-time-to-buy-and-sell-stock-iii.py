0391417621deba3a1d39508193ae68d49383db11
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-22 19:28:06 +0530

    Time: 2103 ms (5.01%) | Memory: 620.5 MB (5.03%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        @cache
+        def dfs(i,state,no_of_trans):
+            if no_of_trans>=2:
+                return 0
+            
+            if i>=len(prices):
+                return 0
+            
+            profit=0
+
+            if state==0:
+                buy=-prices[i]+dfs(i+1,1,no_of_trans)
+                skip=dfs(i+1,state,no_of_trans)
+                profit=max(buy,skip)
+            elif state==1:
+                sell=prices[i]+dfs(i+1,0,no_of_trans+1)
+                skip=dfs(i+1,state,no_of_trans)
+                profit=max(sell,skip)
+            
+            return profit
+        
+        return dfs(0,0,0)
