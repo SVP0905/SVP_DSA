58c7790087703a548d88e3c1b4077413933d3a0a
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-22 20:15:49 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (96.12%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,26 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        memo={}
-        def dfs(i,state):
-            if i>=len(prices):
-                return 0
-            
-            if (i,state) in memo:
-                return memo[(i,state)]
-                
-            if state==0:
-                buy=-prices[i]+dfs(i+1,1)
-                skip=dfs(i+1,state)
-                memo[(i,state)]=max(buy,skip)
-            elif state==1:
-                sell=prices[i]+dfs(i+1,2)
-                skip=dfs(i+1,state)
-                memo[(i,state)]=max(sell,skip)
-            else:
-                memo[(i,state)]=dfs(i+1,0)
-            
+        n=len(prices)
+
+        dp=[[0]*3 for _ in range(n+1)]
+
+        for i in range(n-1,-1,-1):
+
+            #buy
+            buy=-prices[i]+dp[i+1][1]
+            skip=dp[i+1][0]
+            dp[i][0]=max(buy,skip)
+
+            #sell
+            sell=prices[i]+dp[i+1][2]
+            skip=dp[i+1][1]
+            dp[i][1]=max(sell,skip)
+
+            #cooldown
+            dp[i][2]=dp[i+1][0]
 
 
-            return memo[(i,state)]
-        
-        return dfs(0,0)
+        return dp[0][0]
