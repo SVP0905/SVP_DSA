fc09dea0d9024b45a407285a6b870361adca88d4
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-23 18:56:20 +0530

    Time: 1 ms (34.80%) | Memory: 17.5 MB (97.01%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -3,19 +3,21 @@ class Solution:
         m,n=len(obstacleGrid),len(obstacleGrid[0])
         if obstacleGrid[0][0]==1:
             return 0
-            
-        directions=[(0,1),(1,0)]
+
         @cache
         def dfs(i,j):
+            if i>=m or j>=n:
+                return 0
+
+            if obstacleGrid[i][j]==1:
+                return 0
+
             if i==m-1 and j==n-1:
                 return 1
             
-            
             path=0
-            for dr,dc in directions:
-                new_dr,new_dc=dr+i,dc+j
-                if 0<=new_dr<m and 0<=new_dc<n and obstacleGrid[new_dr][new_dc]==0:
-                    path+=dfs(new_dr,new_dc)
+            path+=dfs(i+1,j)
+            path+=dfs(i,j+1)
             
             return path
         
