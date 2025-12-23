021792ebeb5f0bced75610491a7412a6b37a0c21
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-23 18:50:36 +0530

    Time: 3 ms (23.06%) | Memory: 17.9 MB (43.42%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,17 +1,18 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        directions=[(1,0),(0,1)]
 
         @cache
         def dfs(i,j):
             if i==m-1 and j==n-1:
                 return 1
             
+            if i>m or j>n:
+                return 0
+                
             path=0
-            for dr,dc in directions:
-                new_dr,new_dc=i+dr,j+dc
-                if 0<=new_dr<m and 0<=new_dc<=n:
-                    path+=dfs(new_dr,new_dc)
+            
+            path+=dfs(i+1,j)
+            path+=dfs(i,j+1)
             
             return path
         
