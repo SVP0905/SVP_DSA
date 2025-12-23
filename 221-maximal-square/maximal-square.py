9fde036e1d652c1aa2b58f8b7d6390afbe81298a
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-23 23:58:27 +0530

    Time: 368 ms (14.87%) | Memory: 119.8 MB (6.82%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -1,16 +1,23 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         m,n=len(matrix),len(matrix[0])
-        dp=[[0]*n for _ in range(m)]
-        max_=0
+        @cache
+        def dfs(i,j):
+            if i>=m or j>=n:
+                return 0
+            
+            if matrix[i][j]=='0':
+                return 0
+            
+            area=0
+            if matrix[i][j]=='1':
+                area=1+min(dfs(i+1,j),dfs(i+1,j+1),dfs(i,j+1))
+            return area
+        
+        
+        res=float('-inf')
         for i in range(m):
             for j in range(n):
-                if matrix[i][j]=='1':
-                    if i==0 or j==0:
-                        dp[i][j]=1
-                    else:
-                        dp[i][j]=min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
-                    
-                    max_=max(max_,dp[i][j])
+                res=max(res,dfs(i,j))
         
-        return (max_**2)
+        return res*res
