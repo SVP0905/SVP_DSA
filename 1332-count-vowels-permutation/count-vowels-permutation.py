1f8769fa2f3f37235bc535b7304f1f5d05b358b0
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-23 12:07:22 +0530

    Time: 1863 ms (5.10%) | Memory: 62.5 MB (26.91%) - LeetSync

--- 1332-count-vowels-permutation/count-vowels-permutation.py
+++ 1332-count-vowels-permutation/count-vowels-permutation.py
@@ -2,28 +2,31 @@ class Solution:
     def countVowelPermutation(self, n: int) -> int:
         MOD=10**9+7
         vowels=set('aeiou')
-        transitions={
-            'a':['e'],
-            'e':['a','i'],
-            'i':['a','e','o','u'],
-            'o':['i','u'],
-            'u':['a']
-        }
-        
+
         @cache
         def dfs(i,last_ch):
             if i==n:
                 return 1
             
             cnt=0
-            for ch in transitions[last_ch]:
-                cnt=(cnt+dfs(i+1,ch))%MOD
-            
-            return cnt
+            for ch in vowels:
+                if last_ch==None:
+                    cnt=(cnt+dfs(i+1,ch))%MOD
+                
+                if last_ch=='a' and ch=='e':
+                    cnt=(cnt+dfs(i+1,ch))%MOD
+                
+                if last_ch=='e' and (ch=='a' or ch=='i'):
+                    cnt=(cnt+dfs(i+1,ch))%MOD
+                
+                if last_ch=='i' and ch!='i':
+                    cnt=(cnt+dfs(i+1,ch))%MOD
+                
+                if last_ch=='o' and (ch=='i' or ch=='u'):
+                    cnt=(cnt+dfs(i+1,ch))%MOD
+                
+                if last_ch=='u' and ch=='a':
+                    cnt=(cnt+dfs(i+1,ch))%MOD
+            return cnt%MOD
         
-
-        res=0
-        for ch in vowels:
-            res=(res+dfs(1,ch))%MOD
-        
-        return res
+        return dfs(0,None)%MOD
