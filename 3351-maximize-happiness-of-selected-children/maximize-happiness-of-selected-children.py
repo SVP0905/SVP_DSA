a309f92353f31b606e9f8553f0691c20ed84cce3
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-25 10:33:22 +0530

    Time: 816 ms (66.67%) | Memory: 43.2 MB (97.16%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
@@ -3,19 +3,7 @@ class Solution:
         n=len(happiness)
         happiness.sort(reverse=True)
         res=0
-        # for i in range(k):
-        #     res+=happiness[i]
-        #     print(happiness)
-        #     for j in range(i+1,n):
-        #         happiness[i]-=1 if happiness[i]-1>=0 else 0
-        #     print(happiness)
-        
-        # return res
-
         for i in range(k):
-            if i==0:
-                res+=happiness[i]
-            else:
-                res+=happiness[i]-i if happiness[i]-i>=0 else 0
+            res+=happiness[i]-i if happiness[i]-i>=0 else 0
         
         return res
