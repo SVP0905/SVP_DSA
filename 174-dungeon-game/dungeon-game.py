2ada7312e8ef477916a389d6ec999694a331956e
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-25 11:18:31 +0530

    Time: 3 ms (96.89%) | Memory: 18.1 MB (96.72%) - LeetSync

--- 174-dungeon-game/dungeon-game.py
+++ 174-dungeon-game/dungeon-game.py
@@ -2,14 +2,32 @@ class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
 
         m,n=len(dungeon),len(dungeon[0])
-        @cache
-        def dfs(i,j):
-            if i==m-1 and j==n-1:
-                return max(1,1-dungeon[i][j])
+        # @cache
+        # def dfs(i,j):
+        #     if i==m-1 and j==n-1:
+        #         return max(1,1-dungeon[i][j])
             
-            if i>=m or j>=n:
-                return float('inf')
+        #     if i>=m or j>=n:
+        #         return float('inf')
             
-            return max(1,min(dfs(i+1,j),dfs(i,j+1))-dungeon[i][j])
+        #     return max(1,min(dfs(i+1,j),dfs(i,j+1))-dungeon[i][j])
         
-        return dfs(0,0)
+        # return dfs(0,0)
+
+        dp=[[0]*(n+1) for _ in range(m+1)]
+
+        for i in range(m):
+            dp[i][n]=float('inf')
+        
+        for j in range(n):
+            dp[m][j]=float('inf')
+        
+
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                if i==m-1 and j==n-1:
+                    dp[i][j]=max(1,1-dungeon[i][j])
+                else:
+                    dp[i][j]=max(1,min(dp[i+1][j],dp[i][j+1])-dungeon[i][j])
+        
+        return dp[0][0]
