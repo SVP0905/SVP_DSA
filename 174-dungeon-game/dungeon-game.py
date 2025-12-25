db83bb28655f6393df24b5997472592ccee0993d
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-25 11:14:24 +0530

    Time: 15 ms (14.88%) | Memory: 20.8 MB (9.40%) - LeetSync

--- 174-dungeon-game/dungeon-game.py
+++ 174-dungeon-game/dungeon-game.py
@@ -1,27 +1,15 @@
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+
         m,n=len(dungeon),len(dungeon[0])
-        
         @cache
-        def dfs(i,j,health):
+        def dfs(i,j):
+            if i==m-1 and j==n-1:
+                return max(1,1-dungeon[i][j])
+            
             if i>=m or j>=n:
-                return False
-
-            health+=dungeon[i][j]
-
-            if health<=0:
-                return False
+                return float('inf')
             
-            if i==m-1 and j==n-1 and health>0:
-                return True
-            
-            if dfs(i+1,j,health) or dfs(i,j+1,health):
-                return True
-            
-            return False
-
-
-        for hp in range(1,100000):
-            if dfs(0,0,hp):
-                return hp
-
+            return max(1,min(dfs(i+1,j),dfs(i,j+1))-dungeon[i][j])
+        
+        return dfs(0,0)
