41d6b71d1922140d0b1f3ecfbc64b5e639ae3b70
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-26 17:24:37 +0530

    Time: 552 ms (5.04%) | Memory: 75.9 MB (5.04%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -1,34 +1,24 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         m,n=len(matrix),len(matrix[0])
-        indegree=[[0]*n for _ in range(m)]
-
         directions=[(0,1),(1,0),(0,-1),(-1,0)]
+        @cache
+        def dfs(i,j,prev):
+            if i<0 or j<0 or i>=m or j>=n:
+                return 0
+            if matrix[i][j]<prev:
+                return 0
+            
+            cnt=float('-inf')
+            for dr,dc in directions:
+                if matrix[i][j]>prev:
+                    cnt=max(cnt,1+dfs(i+dr,j+dc,matrix[i][j]))
+            
+            return cnt
+        
+        res=float('-inf')
         for i in range(m):
             for j in range(n):
-                for dr,dc in directions:
-                    new_dr,new_dc=dr+i,dc+j
-                    if (0<=new_dr<m and 0<=new_dc<n and matrix[new_dr][new_dc]>matrix[i][j]):
-                        indegree[new_dr][new_dc]+=1
+                res=max(res,dfs(i,j,-1))
         
-        q=deque()
-        for i in range(m):
-            for j in range(n):
-                if indegree[i][j]==0:
-                    q.append((i,j))
-
-        
-        LIS=0
-        while q:
-
-            for _ in range(len(q)):
-                x,y=q.popleft()
-                for dr,dc in directions:
-                    new_dr,new_dc=dr+x,dc+y
-                    if (0<=new_dr<m and 0<=new_dc<n and matrix[new_dr][new_dc]>matrix[x][y]):
-                        indegree[new_dr][new_dc]-=1
-                        if indegree[new_dr][new_dc]==0:
-                            q.append((new_dr,new_dc))
-            LIS+=1
-
-        return LIS
+        return res
