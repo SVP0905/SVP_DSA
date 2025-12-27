ce6af9066b0e013ff2d0e4c64dffa3508c11c6b3
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-27 19:23:22 +0530

    Time: 47 ms (6.25%) | Memory: 26.1 MB (5.32%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.py
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.py
@@ -1,20 +1,13 @@
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
-        total=sum(stones)
-        target=total//2
-
-        dp=[[False]*(target+1) for _ in range(len(stones)+1)]
-        dp[0][0]=True
-
-        for i in range(1,len(stones)+1):
-            for j in range(target+1):
-                if j<stones[i-1]:
-                    dp[i][j]=dp[i-1][j]
-                else:
-                    dp[i][j]=dp[i-1][j] or dp[i-1][j-stones[i-1]]
-        
-        for j in range(target,-1,-1):
-            if dp[len(stones)][j]:
-                return total-2*j
+        @cache
+        def dfs(i,cur_val):
+            if i>=len(stones):
+                return abs(cur_val)
             
-        return total
+            add=dfs(i+1,cur_val+stones[i])
+            sub=dfs(i+1,cur_val-stones[i])
+
+            return min(add,sub)
+        
+        return dfs(0,0)
