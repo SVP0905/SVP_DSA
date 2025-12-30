4c9ea69f556e11e4cc34821e6af137273ddad5ed
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-31 00:18:47 +0530

    Time: 127 ms (58.00%) | Memory: 17.6 MB (95.48%) - LeetSync

--- 321-create-maximum-number/create-maximum-number.py
+++ 321-create-maximum-number/create-maximum-number.py
@@ -1,9 +1,8 @@
 class Solution:
     def maxNumber(self, nums1: List[int], nums2: List[int], k: int) -> List[int]:
-        def max_seq(seq,length):
-            x=len(seq)
+        def most_competive_seq(seq,length):
             stack=[]
-            map_={num:i for i,num in enumerate(seq)}
+            x=len(seq)
             for i,num in enumerate(seq):
                 while stack and num>stack[-1] and len(stack)-1+(x-i)>=length:
                     stack.pop()
@@ -23,20 +22,18 @@ class Solution:
                     res.append(seq2.pop(0))
             
             return res
+        
 
         m,n=len(nums1),len(nums2)
         min_=max(0,k-n)
         max_=min(m,k)
         ans=[]
         for i in range(min_,max_+1):
-            seq1=max_seq(nums1,i)
-            seq2=max_seq(nums2,k-i)
+            seq1=most_competive_seq(nums1,i)
+            seq2=most_competive_seq(nums2,k-i)
 
             cur_res=merge(seq1,seq2)
             if cur_res>ans:
                 ans=cur_res
         
         return ans
-
-
-
