b997deff70d698f78b2c7f975742e152022b846c
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2025-12-31 17:59:12 +0530

    Time: 2858 ms (23.34%) | Memory: 17.4 MB (98.71%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.py
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.py
@@ -1,24 +1,13 @@
 class Solution:
     def minCut(self, s: str) -> int:
-        res=0
+        n=len(s)
+        dp=[float('inf')]*(n+1)
+        dp[n]=-1
 
-        @cache
-        def dfs(start_idx):
-            if start_idx==len(s):
-                #if we reached the end then we need 0 cuts
-                return 0
-            
-            mincuts=float('inf')
-            for j in range(start_idx,len(s)):
-                chunk=s[start_idx:j+1]
-                if chunk==chunk[::-1]:
-                    if j==len(s)-1:
-                        mincuts=0
-                    else:
-                        cuts=1+dfs(j+1)
-                        mincuts=min(mincuts,cuts)
-
-            
-            return mincuts
+        for i in range(n-1,-1,-1):
+            for j in range(1,n-i+1):
+                substr=s[i:i+j]
+                if substr==substr[::-1]:
+                    dp[i]=min(dp[i],1+dp[i+j])
         
-        return dfs(0)
+        return dp[0]
