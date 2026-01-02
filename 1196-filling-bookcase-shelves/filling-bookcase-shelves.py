376a26ccfde9884df7fed5e38a591c432f8d82b6
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2026-01-02 10:35:01 +0530

    Time: 3 ms (68.06%) | Memory: 17.8 MB (87.22%) - LeetSync

--- 1196-filling-bookcase-shelves/filling-bookcase-shelves.py
+++ 1196-filling-bookcase-shelves/filling-bookcase-shelves.py
@@ -1,16 +1,14 @@
 class Solution:
     def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
-
         n=len(books)
-        @cache
-        def dfs(i):
-            if i==n:
-                return 0
-            
+        dp=[float('inf')]*(n+1)
+
+        dp[n]=0
+
+
+        for i in range(n-1,-1,-1):
             cur_width=0
             max_height_on_cur_shelf=0
-            min_total_height=float('inf')
-
             for j in range(i,n):
                 book_w,book_h=books[j]
 
@@ -20,10 +18,10 @@ class Solution:
                 if cur_width>shelfWidth:
                     break
                 
-                cur_shelf_cost=max_height_on_cur_shelf+dfs(j+1)
+                dp[i]=min(dp[i],max_height_on_cur_shelf+dp[j+1])
 
-                min_total_height=min(cur_shelf_cost,min_total_height)
+                
+        
+        return dp[0]
 
-            return min_total_height
-            
-        return dfs(0)
+
