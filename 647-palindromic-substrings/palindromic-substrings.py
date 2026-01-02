db7604eb47dc12dccf404a4692e326410cf2ffad
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2026-01-03 02:24:38 +0530

    Time: 1075 ms (5.01%) | Memory: 334.2 MB (5.00%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,29 +1,23 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
+        @cache
+        def isPalindrome(i,j):
+            if i>=j:
+                return True
+            
+            if i==j:
+                return False
+            
+            if s[i]==s[j]:
+                return isPalindrome(i+1,j-1)
+            
+            return False
         
-        n = len(s)
-
-        dp = [[False] * n for x in range(n)]
-
-        count = 0
-
-        for x in range(n):
-            dp[x][x] = True
-            count += 1
-
-        for x in range(n-1):
-            if(s[x] == s[x+1]):
-                dp[x][x+1] = True
-                count += 1
-
+        n=len(s)
+        res=0
+        for i in range(n):
+            for j in range(i,n):
+                if isPalindrome(i,j):
+                    res+=1
         
-        for length in range(3,n+1):
-
-            for i in range(n-length+1):
-                j = i + length - 1
-
-                if(s[i] == s[j] and dp[i+1][j-1]):
-                    dp[i][j] = True
-                    count += 1
-        
-        return count
+        return res
