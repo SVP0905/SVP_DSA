34231891725bbe807e9951623155d75b23bf68c3
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2026-01-13 11:22:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (75.76%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.py
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.py
@@ -1,13 +1,3 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        A_u=edges[0][0]
-        A_v=edges[0][1]
-
-        B_u=edges[1][0]
-        B_v=edges[1][1]
-
-        if A_u==B_u or A_u==B_v:
-            return A_u
-        else:
-            return A_v
-            
+        return (set(edges[0]) & set(edges[1])).pop()
