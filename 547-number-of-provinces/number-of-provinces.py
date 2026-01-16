25357f5f58169d951b5d8390bd2c0b6d1f18b52f
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2026-01-16 11:13:57 +0530

    Time: 11 ms (25.58%) | Memory: 20.3 MB (13.53%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,26 +1,35 @@
 class DSU:
     def __init__(self,n):
-        self.parent=[0]*(n+1)
-        self.rank=[0]*(n+1)
-        for i in range(n+1):
-            self.parent[i]=i
+        self.parent=[i for i in range(n+1)]
+        self.size=[1]*(n+1)
+        self.num_comps=n
     
+
     def find(self,u):
         if u==self.parent[u]:
             return u
-        return self.find(self.parent[u])
+        
+        self.parent[u]=self.find(self.parent[u])
+
+        return self.parent[u]
     
 
     def union(self,u,v):
         pu,pv=self.find(u),self.find(v)
 
-        if self.rank[pu]<self.rank[pv]:
+        if pu==pv:
+            return False
+        
+        if self.size[pu]<self.size[pv]:
             self.parent[pu]=pv
-        elif self.rank[pv]<self.rank[pu]:
-            self.parent[pv]=pu
+            self.size[pv]+=self.size[pu]
         else:
             self.parent[pv]=pu
-            self.rank[pu]+=1
+            self.size[pu]+=self.size[pv]
+        
+
+        self.num_comps-=1
+        return True
     
 
 
@@ -28,12 +37,17 @@ class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n=len(isConnected)
         dsu=DSU(n)
+
         for i in range(n):
             for j in range(n):
                 if isConnected[i][j]==1:
                     dsu.union(i,j)
         
-        res=set()
-        for i in range(n):
-            res.add(dsu.find(i))
-        return len(res)
+        return dsu.num_comps
+        
+
+
+
+            
+
+        
