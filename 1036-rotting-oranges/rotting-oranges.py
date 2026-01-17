d12fbe4f1082fd29dc5a35036759ff6b4bfcf8ed
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2026-01-17 09:40:06 +0530

    Time: 3 ms (80.42%) | Memory: 19.3 MB (13.88%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,33 +1,32 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         m,n=len(grid),len(grid[0])
-        directions=[(0,1),(1,0),(0,-1),(-1,0)]
-        fresh=0
         q=deque()
+        fresh=0
         for i in range(m):
             for j in range(n):
-                if grid[i][j]==1:
-                    fresh+=1
                 if grid[i][j]==2:
                     q.append((i,j))
-        res=0
+                elif grid[i][j]==1:
+                    fresh+=1
+        
+        if fresh==0:
+            return 0
+
+        directions=[(0,1),(1,0),(0,-1),(-1,0)]
+        time=0
         while q:
-            q_len=len(q)
-            rotten=False
-            for _ in range(q_len):
-                x,y=q.popleft()
+            for _ in range(len(q)):
+                r,c=q.popleft()
                 for dr,dc in directions:
-                    new_dr,new_dc=dr+x,dc+y
-                    if 0<=new_dr<m and 0<=new_dc<n and grid[new_dr][new_dc]==1:
-                        grid[new_dr][new_dc]=2
+                    nr,nc=r+dr,dc+c
+                    if 0<=nr<m and 0<=nc<n and grid[nr][nc]==1:
+                        grid[nr][nc]=2
                         fresh-=1
-                        q.append((new_dr,new_dc))
-                        rotten=True
+                        q.append((nr,nc))
+            time+=1
 
-            if rotten==True:
-                res+=1
-        
-        return res if fresh==0 else -1
-                    
-
-        
+        if fresh==0:
+            return time-1
+        else:
+            return -1
