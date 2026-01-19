fc3a0ad3ebe18fdfebabbbcbf31e72fed4da3d4f
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2026-01-19 20:16:11 +0530

    Time: 34 ms (89.92%) | Memory: 21.7 MB (31.52%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
@@ -1,35 +1,23 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
-        supplies=set(supplies)
-        recipe_to_idx = {recipe: i for i, recipe in enumerate(recipes)}
-        adj={i:[] for i in range(len(recipes))}
-        indegree=[0]*len(recipes)
-        res=[]
+        graph=defaultdict(list)
+        indegree=defaultdict(int)
+        available=set(supplies)
 
-        for i in range(len(recipes)):
-            for ingredient in ingredients[i]:
-                if ingredient not in supplies:
-                    if ingredient in recipe_to_idx:
-                        j=recipe_to_idx[ingredient]
-                        adj[j].append(i)
-                        indegree[i]+=1
-                    else:
-                        indegree[i]=1000
+        for i,r in enumerate(recipes):
+            for ing in ingredients[i]:
+                if ing not in available:
+                    graph[ing].append(r)
+                    indegree[r]+=1
         
-        q=deque()
-        for i in range(len(recipes)):
-            if indegree[i]==0:
-                q.append(i)
-        
+        q=deque([r for r in recipes if indegree[r]==0])
+        res=[]
         while q:
             node=q.popleft()
-            res.append(recipes[node])
-            for nei in adj[node]:
+            res.append(node)
+            for nei in graph[node]:
                 indegree[nei]-=1
                 if indegree[nei]==0:
                     q.append(nei)
         
-                
         return res
-        
-        
