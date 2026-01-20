50c6390d42d4a558e51ba1a1063c2baaa2525198
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2026-01-20 19:44:11 +0530

    Time: 146 ms (57.28%) | Memory: 20.3 MB (63.07%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -1,23 +1,34 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         m,n=len(matrix),len(matrix[0])
+        indegree=[[0]*n for _ in range(m)]
+
         directions=[(0,1),(1,0),(0,-1),(-1,0)]
 
-        @cache
-        def dfs(i,j):
-            cnt=1
-            for dr,dc in directions:
-                nr,nc=dr+i,dc+j
-                if 0<=nr<m and 0<=nc<n and matrix[nr][nc]>matrix[i][j]:
-                    cnt=max(cnt,1+dfs(nr,nc))
-            
-            return cnt
-        
-
-        res=0
         for i in range(m):
             for j in range(n):
-                res=max(res,dfs(i,j))
+                for dr,dc in directions:
+                    nr,nc=dr+i,dc+j
+                    if 0<=nr<m and 0<=nc<n and matrix[nr][nc]>matrix[i][j]:
+                        indegree[nr][nc]+=1
+        
+        q=deque()
+        for i in range(m):
+            for j in range(n):
+                if indegree[i][j]==0:
+                    q.append((i,j))
+        
+        res=0
+        while q:
+            res+=1
+            for _ in range(len(q)):
+                r,c=q.popleft()
+                for dr,dc in directions:
+                    nr,nc=dr+r,dc+c
+                    if 0<=nr<m and 0<=nc<n and matrix[nr][nc]>matrix[r][c]:
+                        indegree[nr][nc]-=1
+                        if indegree[nr][nc]==0:
+                            q.append((nr,nc))
+        
         
         return res
-            
