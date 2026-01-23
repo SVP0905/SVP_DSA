89f5f4adfd557c1371e15b34bc8cc325dd6cda3e
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2026-01-23 19:26:10 +0530

    Time: 34 ms (78.68%) | Memory: 32.7 MB (64.90%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
@@ -1,41 +1,38 @@
-class DisjointSetUnion:
+class DSU:
     def __init__(self,n):
-        self.rank=[0]*(n+1)
-        self.parent=[0]*(n+1)
-        for i in range(n+1):
-            self.parent[i]=i
-        
+        self.parent=[i for i in range(n)]
+        self.size=[1]*n
+    
     def find(self,u):
         if u==self.parent[u]:
             return u
-        else:
-            self.parent[u]=self.find(self.parent[u])
-            return self.parent[u]
+        self.parent[u]=self.find(self.parent[u])
+        return self.parent[u]
     
     def union(self,u,v):
         pu,pv=self.find(u),self.find(v)
-        if pu==pv:
-            return
-        elif self.rank[pu]<self.rank[pv]:
+        if pv==pu:
+            return False
+        
+        if self.size[pu]<self.size[pv]:
             self.parent[pu]=pv
-        elif self.rank[pv]<self.rank[pu]:
-            self.parent[pv]=pu
+            self.size[pv]+=self.size[pu]
         else:
-            self.parent[pu]=pv
-            self.rank[pv]+=1
-    
+            self.parent[pv]=pu
+            self.size[pu]+=self.size[pv]
+        return True
+
+
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
         if len(connections)<n-1:
             return -1
 
-        DSU=DisjointSetUnion(n)
-
+        dsu=DSU(n)
+        num_comp=n
         for u,v in connections:
-            DSU.union(u,v)
+            if dsu.union(u,v):
+                num_comp-=1
         
-        components=set()
-        for i in range(n):
-            components.add(DSU.find(i))
-
-        return len(components)-1
+        return num_comp-1
+        
