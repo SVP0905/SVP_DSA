0f33712e9281ec660e33a2e046016936fdd4bab7
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2026-01-29 21:10:46 +0530

    Time: 19 ms (32.72%) | Memory: 21.8 MB (26.58%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -2,15 +2,20 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m,n=len(grid),len(grid[0])
 
-        @cache
-        def dfs(i,j):
-            if i>=m or j>=n:
-                return float('inf')
-            
-            if i==m-1 and j==n-1:
-                return grid[i][j]
-            
-            return min(grid[i][j]+dfs(i+1,j),grid[i][j]+dfs(i,j+1))
+        cost=[[float('inf')]*n for _ in range(m)]
 
+        cost[-1][-1]=grid[-1][-1]
+
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                if i==m-1 and j==n-1:
+                    continue
+                #move right
+                if j+1<n:
+                    cost[i][j]=min(cost[i][j],cost[i][j+1]+grid[i][j])
+                
+                #move down
+                if i+1<m:
+                    cost[i][j]=min(cost[i][j],cost[i+1][j]+grid[i][j])
         
-        return dfs(0,0)
+        return cost[0][0]
