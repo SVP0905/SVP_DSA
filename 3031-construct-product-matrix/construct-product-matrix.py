ad5f7cb8a9042e4eaa722e87b14fc1978d9e5486
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2026-03-24 11:10:03 +0530

    Time: 111 ms (71.52%) | Memory: 43 MB (62.91%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.py
+++ 3031-construct-product-matrix/construct-product-matrix.py
@@ -3,27 +3,28 @@ class Solution:
         MOD=12345
         n,m=len(grid),len(grid[0])
 
-        prefix=[[1]*m for _ in range(n)]
-        suffix=[[1]*m for _ in range(n)]
-
-        cur_prefix=1
-        for i in range(n):
-            for j in range(m):
-                prefix[i][j]=cur_prefix
-                cur_prefix=(cur_prefix*grid[i][j])%MOD
-
-
-        cur_suffix=1
-        for i in range(n-1,-1,-1):
-            for j in range(m-1,-1,-1):
-                suffix[i][j]=cur_suffix
-                cur_suffix=(cur_suffix*grid[i][j])%MOD
-        
+        # prefix=[[1]*m for _ in range(n)]
+        # suffix=[[1]*m for _ in range(n)]
 
         prod=[[1]*m for _ in range(n)]
+        cur_prod=1
+
         for i in range(n):
             for j in range(m):
-                prod[i][j]=(prefix[i][j]*suffix[i][j])%MOD
+                prod[i][j]=cur_prod
+                cur_prod=(cur_prod*grid[i][j])%MOD
+
+
+        cur_prod=1
+        for i in range(n-1,-1,-1):
+            for j in range(m-1,-1,-1):
+                prod[i][j]=(prod[i][j]*cur_prod)%MOD
+                cur_prod=(cur_prod*grid[i][j])%MOD
+        
+
+        # for i in range(n):
+        #     for j in range(m):
+        #         prod[i][j]=(prefix[i][j]*suffix[i][j])%MOD
         
         return prod
                     
