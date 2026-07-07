a5c92e3a74a91d80422bcb4b960844f83b914073
SVP0905 <168821151+SVP0905@users.noreply.github.com>
2026-07-07 11:14:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (14.68%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.py
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.py
@@ -1,21 +1,35 @@
 class Solution:
     def sumAndMultiply(self, n: int) -> int:
-        x=0
-        sum_=0
-        place=1
 
-        while n:
-            digit=n%10
+        #math way using place value
+        # x=0
+        # sum_=0
+        # place=1
+
+        # while n:
+        #     digit=n%10
             
-            if digit!=0:
-                x=(digit*place)+x
-                sum_+=digit
-                place*=10
-            n=n//10
+        #     if digit!=0:
+        #         x=(digit*place)+x
+        #         sum_+=digit
+        #         place*=10
+        #     n=n//10
         
 
-        print(sum_)
-        print(x)
+        # print(sum_)
+        # print(x)
+
+        # return x*sum_
+
+        #strings way (pythonic way)
+        digits=[ch for ch in str(n) if ch!='0']
+
+        print(digits)
+        if not digits:
+            return 0
+        
+        sum_=sum(int(d) for d in digits)
+        x=int(''.join(digits))
 
         return x*sum_
         
